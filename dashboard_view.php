bed3b116f62fefc9532052aa13e40c6a064a60e9
frggycc <charliecardenas.pro@gmail.com>
2026-04-19 22:30:35 -0700

    Added simple nav menu to test logout

--- dashboard_view.php
+++ dashboard_view.php
@@ -1,11 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <title>Titan Court Tracker - Dashboard</title>
-  <link rel="stylesheet" href="css/style.css">
+    <meta charset="UTF-8">
+    <title>Titan Court Tracker - Dashboard</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
-    <h1>Hello World</h1>
+  <header>
+    <h1>Titan Court Tracker</h1>
+    <nav>
+      <a href="dashboard.php">Home</a>
+      <a href="">Account</a>
+      <a href="logout.php">Logout</a>
+    </nav>
+  </header>
 </body>
 </html>
