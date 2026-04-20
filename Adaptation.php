70e425c6b5d57005c01afa2aa1e29c1e849861cd
frggycc <charliecardenas.pro@gmail.com>
2026-04-19 20:04:43 -0700

    Changed DB host for seperately installed DB from XAMPP

--- Adaptation.php
+++ Adaptation.php
@@ -1,6 +1,6 @@
 <?php
     define('DATA_BASE_NAME', 'TitanCourtTracker');
-    define('DATA_BASE_HOST', 'localhost');
+    define('DATA_BASE_HOST', '127.0.0.1');
 
     // Default role to use when not logged in, such as to register a new user or to get end-user credentials
     define('NO_ROLE', 'Executive Manager');
