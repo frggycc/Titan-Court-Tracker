11a2f83d56118d8241e4b5c5d12c9f679401d3f9
frggycc <charliecardenas.pro@gmail.com>
2026-04-22 15:57:03 -0700

    Seperate file created for header; Possible footer

---
+++ components/header.php
@@ -0,0 +1,8 @@
+<header>
+    <h1>Titan Court Tracker</h1>
+    <nav>
+        <a href="landing.php">Home</a>
+        <a href="">Account</a>
+        <a href="logout.php">Logout</a>
+    </nav>
+</header>
